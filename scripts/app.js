99600c13e7b7ebbf0a54f4c97660d317f1b84e1b
Jonah <schieldjt@s.dcsdk12.org>
2022-11-30 15:41:45 -0700

    added game, player, segment classes

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,42 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+let game = {
+    gridSize: 20,
+    refreshRate: 500, // milliseconds
+};
+
+class Player {
+    /**
+     * @param {any} x
+     * @param {any} y
+     */
+    constuctor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.w = game.gridSize;
+        this.h = this.w;
+        this.segments = [];
+    }
+}
+
+class Segment {    /**
+* @param {number} x
+* @param {number} y
+* @param {string} color
+*/
+constuctor(x, y, color) {
+   this.x = x;
+   this.y = y;
+   this.w = game.gridSize;
+   this.h = this.w;
+   this.color = color;
+    }
+
+    update() {}
+    
+    draw() {}
+    
+    }
+}
